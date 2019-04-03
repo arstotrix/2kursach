0236ae441b9e38080026ad4840b63248aabd3a50
Marie <mashamaslova85@gmail.com>
2019-04-03 17:50:23 +0300

    03.04. xmlfill

--- test01/testing_xml.py
+++ test01/testing_xml.py
@@ -6,7 +6,13 @@ root = tree.getroot()
 #print(root.attrib) #attributes i guess
 
 #print(root[1][1].tag)
-for word in root[1][1].findall('div'):
-    print(word)
+for act in root[1][1]:
+    #print(r.tag, r.attrib)
+    for scene in act[1:]:
+        print(scene.tag, scene.attrib)
+        for part in scene:
+            if str(part.tag) == '{http://www.tei-c.org/ns/1.0}sp':
+                print(part.tag, part.attrib)
+
 
 
