5d2a04f7f0d61dac325b6ffc2fac394dd9c57386
Marie <mashamaslova85@gmail.com>
2019-04-03 16:39:29 +0300

    03.04 tags json

---
+++ _metadata/tags.py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+import json
+from pprint import pprint as pp
+with open ('tags.tsv', encoding='utf-8') as f:
+    text = f.read().split('\n')
+tags = {}
+for t in text:
+    t=t.split('\t')
+    tags[t[0]]=t[1]
+with open("tags.json", 'w', encoding="utf-8") as f:
+    json.dump(tags, f, ensure_ascii=False, indent=2)
+
+
