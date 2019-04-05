d7ba0229760ad7ed02d454654478938f14a6ad33
Marie <mashamaslova85@gmail.com>
2019-04-05 16:15:03 +0300

    04.04. final work

--- _redacted/compiler.py
+++ _redacted/compiler.py
@@ -1,26 +1,36 @@
 import re
 from json import load, dump
 import os
+from bs4 import BeautifulSoup
 
-def body_out(fname):
+def body_out(fname): #достаём тело из текста
     with open(fname, encoding= 'utf-8') as f:
-        text = f.read()
-    return text
+        soup = BeautifulSoup(f.read(), 'lxml')
+    body = soup.find('body')
+    return body
 
-def src(start_path):
+def names(start_path): #ходим по папке и достаём имена
     for root, dirs, files in os.walk(start_path):
         names = files
+    return names
+
+def src_main(names): #достаём имена, ссылка на другую функцию
+    titles = []
     for name in names:
         if name.endswith('.xml'):
-            text = body_out(name)
+            src_bodies(name)
             name = name.strip(".xml")
-            print(name)
-            #print(text[:1000])
+            titles.append(name)
+    return titles
 
-
+def src_bodies(name): #достаём тело и работаем с ними
+    body = body_out(name)
+    ps = body.findall('p')
+    return
 
 def main():
-    src('.')
+    src(names('.'))
+
 
 
 
--- test01/testagain.py
+++ test01/testagain.py
@@ -2,3 +2,7 @@ from bs4 import BeautifulSoup
 with open('Ado.xml', encoding='utf-8') as f:
     text = f.read()
 soup = BeautifulSoup(text, 'lxml')
+body = soup.find('body')
+
+
+print(type(body))
